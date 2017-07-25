cb0459033a7922e76fd9fadff02d1311b1f56611
Tanuj Mishra <thetanuj1@gmail.com>
2017-07-25 12:15:29 +0530

    Fixed a minor bug in models

--- regapp/models.py
+++ regapp/models.py
@@ -89,7 +89,7 @@ class MyUser(AbstractBaseUser):
         default="Coding",
         blank=False,
     )
-    card_ids = models.ArrayField(models.CharField(max_length=50), blank=True)
+    card_ids = ArrayField(models.CharField(max_length=50), blank=True)
     customer_id = models.CharField(max_length=20, blank=True)
     featured_video = models.URLField(blank=True)
     featured_text = models.CharField(max_length=1000, blank=True)
