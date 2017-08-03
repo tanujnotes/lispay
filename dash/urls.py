2dc57058e0d00f339264cb9dc2f7e0edef4eaf2c
Tanuj Mishra <thetanuj1@gmail.com>
2017-08-03 21:09:47 +0530

    Added a dummy dashboard

--- dash/urls.py
+++ dash/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from dash import views
+from regapp.views import show_user_profile
 
 urlpatterns = [
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^(?P<profile_username>[\w\_]+)/$', show_user_profile, name='show_user_profile'),
 ]
--- dash/views.py
+++ dash/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def dashboard(request):
-    return render(request, 'dash/base_dash.html', {})
+    return render(request, 'dash/dashboard.html', {})
