a959cdf3bde397833eb70a96c03b439d6598d168
Tanuj Mishra <thetanuj1@gmail.com>
2017-08-06 14:31:37 +0530

    Adding http in social media profile urls

--- utils.py
+++ utils.py
@@ -16,33 +16,31 @@ def clean_youtube_link(youtube_link):
 
 def get_social_details(request):
     facebook_username = request.POST.get('facebook_username', "")
-    facebook_link = request.POST.get('facebook_link', "")
+    facebook_link = check_http(request.POST.get('facebook_link', ""))
     twitter_username = request.POST.get('twitter_username', "")
-    twitter_link = request.POST.get('twitter_link', "")
+    twitter_link = check_http(request.POST.get('twitter_link', ""))
     instagram_username = request.POST.get('instagram_username', "")
-    instagram_link = request.POST.get('instagram_link', "")
+    instagram_link = check_http(request.POST.get('instagram_link', ""))
     snapchat_username = request.POST.get('snapchat_username', "")
-    snapchat_link = request.POST.get('snapchat_link', "")
+    snapchat_link = check_http(request.POST.get('snapchat_link', ""))
     youtube_username = request.POST.get('youtube_username', "")
-    youtube_link = request.POST.get('youtube_link', "")
+    youtube_link = check_http(request.POST.get('youtube_link', ""))
     twitch_username = request.POST.get('twitch_username', "")
-    twitch_link = request.POST.get('twitch_link', "")
+    twitch_link = check_http(request.POST.get('twitch_link', ""))
     dribble_username = request.POST.get('dribble_username', "")
-    dribble_link = request.POST.get('dribble_link', "")
-    googleplus_username = request.POST.get('googleplus_username', "")
-    googleplus_link = request.POST.get('googleplus_link', "")
+    dribble_link = check_http(request.POST.get('dribble_link', ""))
     linkedin_username = request.POST.get('linkedin_username', "")
-    linkedin_link = request.POST.get('linkedin_link', "")
+    linkedin_link = check_http(request.POST.get('linkedin_link', ""))
     blog_username = request.POST.get('blog_username', "")
-    blog_link = request.POST.get('blog_link', "")
+    blog_link = check_http(request.POST.get('blog_link', ""))
     website_username = request.POST.get('website_username', "")
-    website_link = request.POST.get('website_link', "")
+    website_link = check_http(request.POST.get('website_link', ""))
     merchandise_username = request.POST.get('merchandise_username', "")
-    merchandise_link = request.POST.get('merchandise_link', "")
+    merchandise_link = check_http(request.POST.get('merchandise_link', ""))
     github_username = request.POST.get('github_username', "")
-    github_link = request.POST.get('github_link', "")
+    github_link = check_http(request.POST.get('github_link', ""))
     other_username = request.POST.get('other_username', "")
-    other_link = request.POST.get('other_link', "")
+    other_link = check_http(request.POST.get('other_link', ""))
 
     social_details = {"facebook_details": {"facebook_username": facebook_username,
                                            "facebook_link": facebook_link},
@@ -58,8 +56,6 @@ def get_social_details(request):
                                          "twitch_link": twitch_link},
                       "dribble_details": {"dribble_username": dribble_username,
                                           "dribble_link": dribble_link},
-                      "googleplus_details": {"googleplus_username": googleplus_username,
-                                             "googleplus_link": googleplus_link},
                       "linkedin_details": {"linkedin_username": linkedin_username,
                                            "linkedin_link": linkedin_link},
                       "blog_details": {"blog_username": blog_username,
@@ -74,3 +70,12 @@ def get_social_details(request):
                                         "other_link": other_link}
                       }
     return social_details
+
+
+def check_http(url):
+    url = url.strip()
+    if url is None:
+        return ""
+    if url.startswith('www.'):
+        return "http://" + url
+    return url
