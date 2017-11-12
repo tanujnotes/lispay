4d6cfadd34994df64df23b42ec1b8dbec6e87904
Tanuj Mishra <thetanuj1@gmail.com>
2017-11-12 11:48:07 +0530

    Changes in url format

--- regapp/urls.py
+++ regapp/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     url(r'^search/$', views.search, name='search'),
     url(r'^webhook/$', views.webhook, name='webhook'),
     url(r'^checkout/$', views.checkout, name='checkout'),
-    url(r'^thank_you/$', views.thank_you, name='thank_you'),
+    url(r'^thank-you/$', views.thank_you, name='thank_you'),
     url(r'^login_redirect/$', views.login_redirect, name='login_redirect'),
-    url(r'^show_creators/(?P<category>[\w\-]+)/$', views.show_creators, name='show_creators'),
-    url(r'^show_creators/(?P<category>[\w\-]+)/(?P<page>[0-9]+)/$', views.show_creators, name='show_creators'),
+    url(r'^show-creators/(?P<category>[\w\-]+)/$', views.show_creators, name='show_creators'),
+    url(r'^show-creators/(?P<category>[\w\-]+)/(?P<page>[0-9]+)/$', views.show_creators, name='show_creators'),
     url(r'^(?P<creator>[\w\_]+)/checkout/$', views.checkout, name='checkout'),
     url(r'^(?P<profile_username>[\w\_]+)/$', views.show_user_profile, name='show_user_profile'),
 ]
