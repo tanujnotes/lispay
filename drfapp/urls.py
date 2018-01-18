3f78d64ac5407526bc5d0f626d5c998719135bff
Tanuj Mishra <thetanuj1@gmail.com>
2018-01-18 19:34:43 +0530

    Added get_user api

--- drfapp/urls.py
+++ drfapp/urls.py
@@ -4,4 +4,5 @@ from drfapp import views
 
 urlpatterns = [
     url(r'^get_creators/$', views.get_creators),
+    url(r'^get_user/$', views.get_user),
 ]
--- drfapp/views.py
+++ drfapp/views.py
@@ -12,3 +12,12 @@ def get_creators(request):
     users = MyUser.objects.all()
     serializer = UserSerializer(users, many=True)
     return JsonResponse({"users": serializer.data}, safe=False)
+
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated,))
+def get_user(request):
+    username = request.GET.get('username', '')
+    users = MyUser.objects.get(username=username)
+    serializer = UserSerializer(users, many=False)
+    return JsonResponse(serializer.data, safe=False)
