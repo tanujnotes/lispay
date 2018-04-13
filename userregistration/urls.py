a0db1d2625a95a163bd07152ebc5bfa32ad57a43
Tanuj Mishra <thetanuj1@gmail.com>
2018-04-13 17:15:28 +0530

    Fixed: password reset url not working

--- userregistration/urls.py
+++ userregistration/urls.py
@@ -24,6 +24,7 @@ from regapp import views
 
 urlpatterns = [
                   url(r'^$', views.index, name='index'),
+                  url(r'^', include('django.contrib.auth.urls')),
                   url(r'^admin/', admin.site.urls),
                   url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
                       name="robots_file"),
