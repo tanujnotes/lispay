665d72f73842e79671daa06bde37c4aec3257bcf
Tanuj Mishra <thetanuj1@gmail.com>
2018-04-30 13:34:01 +0530

    Sending category in UserSerializer

--- drfapp/serializers.py
+++ drfapp/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = MyUser
         fields = ('username', 'first_name', 'short_bio', 'email', 'profile_description', 'picture', 'featured_image',
                   'featured_video', 'mobile', 'club_2_reward', 'club_3_reward', 'club_4_reward', 'social_links',
-                  'is_creator', 'featured_text')
+                  'is_creator', 'featured_text', 'category')
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
